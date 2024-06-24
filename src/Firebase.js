afc89b15531766029304abaedcf92be5615845e0
jalal janjua <jalaljanjua88@gmail.com>
2024-06-24 12:25:03 +0500

    modified:   .gitignore 	modified:   src/Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,16 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyD9DNAvs2Ryd-jTweyv2aF7OHegZN0b8JY",
-  authDomain: "my-grocery-home.firebaseapp.com",
-  projectId: "my-grocery-home",
-  storageBucket: "my-grocery-home.appspot.com",
-  messagingSenderId: "888361723877",
-  appId: "1:888361723877:web:930792430b39fbd20df10c",
-  measurementId: "G-QNMK83BJ7K",
-};
+import firebaseConfig from "./FirebaseConfig";
 
 const firebaseApp = initializeApp(firebaseConfig);
 const auth = getAuth(firebaseApp);
