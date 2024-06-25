659d860a816427bfb660407470572faf1005230c
jalal janjua <jalaljanjua88@gmail.com>
2024-06-25 11:19:37 +0500

    modified:   src/FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -7,6 +7,5 @@ const firebaseConfig = {
   appId: process.env.VUE_APP_FIREBASE_APP_ID,
   measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
 };
-console.log("API Key:", process.env.VUE_APP_FIREBASE_API_KEY);
 
 export default firebaseConfig;
