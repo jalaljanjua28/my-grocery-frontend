3885dfab6d11723e1d159bc255a4abaef687e14a
jalal janjua <jalaljanjua88@gmail.com>
2024-06-26 08:29:40 +0500

    modified:   .gitignore 	modified:   app.yaml 	modified:   src/Firebase.js 	modified:   src/FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,11 +1,11 @@
-// const firebaseConfig = {
-//   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-//   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.VUE_APP_FIREBASE_PROJECT_IDY,
-//   storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.VUE_APP_FIREBASE_APP_ID,
-//   measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID,
-// };
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_IDY,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+};
 
-// export default firebaseConfig;
+export default firebaseConfig;
